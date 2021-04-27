343e1404a09305fbc812ed95472af88b98ffb691
byzantineviking <teemu.saravirta@aalto.fi>
2021-04-27 12:14:08 +0300

    Minor fixes

--- backend/edustajat_service/__init__.py
+++ backend/edustajat_service/__init__.py
@@ -1,12 +1,6 @@
 import os
 from flask import Flask
 
-# Take HEROKU PORT
-port = int(os.environ.get("PORT", 5000))
-# Take HEROKU DATABASE_URL
-DATABASE_URL = os.environ['DATABASE_URL']
-
-
 app = Flask(__name__,
             # 1. redirect the default static folder path to point to react
             static_folder='../frontend/build',
--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,13 @@
+import os
 from edustajat_service.db.connect import connect
 from edustajat_service.refine_data.ehdokkaat import refine_ehdokkaat
 from edustajat_service import app, port
-
-
-
+# Take HEROKU PORT
+port = int(os.environ.get("PORT", 5000))
+# Take HEROKU DATABASE_URL
+DATABASE_URL = os.environ['DATABASE_URL']
+print(port)
 if __name__ == '__main__':
     # connect()
-    # refine_ehdokkaat()
+    refine_ehdokkaat()
     # app.run(debug=True, host='0.0.0.0', port=port)
